c2ea8b77ea8eaf67fdc84a0a1ba99bb3daa925a8
youngseon sa <sa@youngseonui-MacBookPro.local>
2026-05-14 02:14:49 +0900

    chore: update firebase config to separate unicon-creator project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0iLbq0spDc9xXAjsbIU_b6pQOVVYMh1A",
-  authDomain: "insurance-crm-31ff4.firebaseapp.com",
-  projectId: "insurance-crm-31ff4",
-  storageBucket: "insurance-crm-31ff4.firebasestorage.app",
-  messagingSenderId: "543855381179",
-  appId: "1:543855381179:web:09396c72ef35125d9ff79e",
-  measurementId: "G-2W186RX2XP"
+  apiKey: "AIzaSyDLHhO9wx8jqItcNoAdKDXvR0BPtr0zB8c",
+  authDomain: "unicon-creator.firebaseapp.com",
+  projectId: "unicon-creator",
+  storageBucket: "unicon-creator.firebasestorage.app",
+  messagingSenderId: "1024367424818",
+  appId: "1:1024367424818:web:f7ead455253cc83a95532b"
 };
 
 // Initialize Firebase
